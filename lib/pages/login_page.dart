2b207826c317ccd3085bcfe4b37a1e9fc3e55e1f
MohsenRahmdeli <mohsen.rahmdeli@gmail.com>
2024-12-18 05:34:34 +0400

    Customize Login Page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,6 +5,33 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.message,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Welcome backm you've been missed!",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
