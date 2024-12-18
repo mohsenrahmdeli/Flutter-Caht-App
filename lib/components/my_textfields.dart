bd0126b325f1b92d5778b44637b309c38e7cd8e5
MohsenRahmdeli <mohsen.rahmdeli@gmail.com>
2024-12-18 05:47:56 +0400

    Customize TextFiled Component

--- lib/components/my_textfields.dart
+++ lib/components/my_textfields.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
 
 class MyTextFields extends StatelessWidget {
-  const MyTextFields({super.key});
+  final String hintText;
+  const MyTextFields({super.key, required this.hintText});
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Theme.of(context).colorScheme.tertiary,
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 25.0,
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Theme.of(context).colorScheme.tertiary,
+            ),
           ),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+          fillColor: Theme.of(context).colorScheme.secondary,
+          filled: true,
+          hintText: hintText,
+          hintStyle: TextStyle(
             color: Theme.of(context).colorScheme.primary,
           ),
         ),
