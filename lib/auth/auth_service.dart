f7a9323835e3fc05fc20461b86022890eb4295d6
MohsenRahmdeli <mohsen.rahmdeli@gmail.com>
2024-12-20 16:30:48 +0400

    Create Login Method

--- lib/auth/auth_service.dart
+++ lib/auth/auth_service.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-class Auth {
+class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<UserCredential> signInWithEmailPassword(String email, password) async {
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../auth/auth_service.dart';
 import '../components/my_button.dart';
 import '../components/my_textfields.dart';
 import 'register_page.dart';
@@ -10,9 +11,22 @@ class LoginPage extends StatelessWidget {
   final void Function()? onTap;
   LoginPage({super.key, required this.onTap});
 
-  void login() {
-    
+  void login(BuildContext context) async {
+    final authService = AuthService();
+
+    try {
+      await authService.signInWithEmailPassword(
+          _emailController.text, _pwController.text);
+    } catch (e) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text(e.toString()),
+        ),
+      );
+    }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,7 +75,6 @@ class LoginPage extends StatelessWidget {
               height: 25,
             ),
             Row(
-              
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
